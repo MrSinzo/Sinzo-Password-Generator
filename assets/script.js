68542e0673e3f579da0a24453b0305682f0aa511
Kyle Feldman <flip93wolf@gmail.com>
2022-10-13 15:14:33 -0700

    Swapped lines 7 and 9 in JS

--- assets/script.js
+++ assets/script.js
@@ -4,20 +4,22 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  passwordText.value = password;//this keeps returning errors stopping my JS from working
-  var password = generatePassword();// this is the function we are creating
+  // passwordText.value = password;//this keeps returning errors, probably stopping my JS from working
+  var password = generatePassword(); // this is the function we are creating
   var passwordText = document.querySelector("#password");
+  passwordText.value = password;//this keeps returning errors, probably stopping my JS from working
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 ///////////////////////// DO NOT CHANGE ABOVE HERE /////////////////////////
-/* 1. prompt asking if you want to generate a password
-   2. prompt for password criteria
+
+/* 1. prompt asking if you want to generate a password on button click
+   2. prompt for password criteria:
     2.1 prompt user asking how long they want their 
     generated password to be (8-128 chars (no less or more)
     2.2 prompt user asking if they want to include uppercase, lowercase, special chars, and numerics
-  3.  Validate input?
+  3. Validate input?
   4. generate a password @ random, within users specifications
   5. When password is generated, display to user, using alert or some kind of insertion through javascript
 */
@@ -29,8 +31,6 @@ var specChars = "~!@#$%^&*_+=><?/|.,"
 function generatePassword() 
 {
   /////////////////////////// WRITE YOUR CODE HERE /////////////////////////
-
-
 
   var btnClicked = function ()
   {confirm("would you like to generate a password");
@@ -44,6 +44,11 @@ function generatePassword()
     writePassword()
   };
 }
+  
+  // document.getElementById("generate").innerHtml = prompt("Would you like to use lower case letters?");
+
+// var btn = document.getElementsByClassName("btn");
+// btn.addEventListener("click", generatePassword);
 
 /*
   prompt?
---
+++ nogoScript.js
@@ -0,0 +1,9 @@
+/*
+var btnClicked = function ()
+{confirm("would you like to generate a password");}
+
+
+btnClicked = document.getElementById("generate").onclick = function() { 
+  writePassword()
+};
+*/
